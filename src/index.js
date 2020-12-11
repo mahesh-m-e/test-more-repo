d34ffefc9743d3399709e6c587c15935c817b64b
mahesh_me <maheswar@contractwrangler.com>
2020-12-11 22:56:20 +0530

    pdf check

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import styles from './styles.module.css'
-import 'antd/dist/antd.css';
-import { Button, Input } from "antd";
 import PdfDocumentEditorComponent from './SyncfusionPdfViewer/index';
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
-
-export const CopyButton = ({text, ...rest}) => <Button {...rest}>{text || 'Button'}</Button>
-
-export const CopyInput = ({...rest}) => <Input {...rest}></Input>;
 
 export const PdfDocumentComponent = ({...rest}) => <PdfDocumentEditorComponent {...rest}/>;
